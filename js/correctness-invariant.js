0dcbfc856f107cc5237b70beacdd673ad70e1ce9
Hardik Patel <hpatel@wsgc.com>
2019-04-15 07:49:06 -0700

    11,768

---
+++ js/correctness-invariant.js
@@ -0,0 +1,14 @@
+const result = (A) => {
+    for (let i = 0; i < A.length; i++) {
+        let value = A[i];
+        let j = i - 1;
+        while (j >= 0 && A[j] > value) {
+            A[j + 1] = A[j];
+            j = j - 1;
+        }
+        A[j + 1] = value;
+        console.log(A)
+    };
+}
+const A = [4, 1, 3, 5, 6, 2];
+result(A);
