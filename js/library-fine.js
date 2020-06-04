2f5b469d90ca1d8ad1b32349c673044ee78f32c6
Hardik Patel <hpatel@wsgc.com>
2020-06-03 21:27:09 -0700

    14,581

--- js/library-fine.js
+++ js/library-fine.js
@@ -1,7 +1,11 @@
 function libraryFine(d1, m1, y1, d2, m2, y2) {
     let fine = 0;
-    if (y1 > y2) {
+    if (y1 < y2) {
+        fine = 0;
+    } else if (y1 > y2) {
         fine = 10000;
+    } else if (m1 < m2) {
+        fine = 0;
     } else if (m1 > m2) {
         fine = (m1 - m2) * 500;
     } else if (d1 > d2) {
@@ -10,3 +14,7 @@ function libraryFine(d1, m1, y1, d2, m2, y2) {
     return fine;
 
 }
+
+console.log(libraryFine(2, 3, 84, 3, 3, 85));
+console.log(libraryFine(2, 7, 1014, 1, 1, 1015));
+console.log(libraryFine(28, 2, 2015, 15, 4, 2015));
