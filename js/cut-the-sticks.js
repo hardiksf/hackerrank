08dd485cfc32ecd34080dcf89e697cb95718c409
Hardik Patel <hpatel@wsgc.com>
2020-06-03 21:02:34 -0700

    14,615

--- js/cut-the-sticks.js
+++ js/cut-the-sticks.js
@@ -1,17 +1,30 @@
 function cutTheSticks(arr) {
-    arr.sort();
-    const result = [arr.length];
-    console.log(arr.length);
-    let min = arr[0];
-    for (let index = 0; index < arr.length; index++) {
-        if (arr[index] === min) {} else {
-            console.log(arr.length - index);
-            result.push(arr.length - index);
-            min = arr[index];
+    arr.sort((a, b) => {
+        return a - b; 
+    });
+    const length = arr.length;
+    const result = [length];
+    while (arr.length > 1) {
+
+        // let count = 1;
+        const firstElement = arr.shift();
+        if (arr[0] === firstElement) {
+            // count++;
+            arr.shift();
+        } if (arr[0] !== firstElement && arr.length !== 0) {
+            result.push(arr.length);
         }
     }
     return result;
 }
 
+// const myArray = [];
 const myArray = [5, 4, 4, 2, 2, 8];
-(cutTheSticks(myArray));
+const myArray2 = [1, 2, 3, 4, 3, 3, 2, 1];
+const myArray3 = [1, 13, 3, 8, 14, 9, 4, 4];
+const myArray4 = [8, 8, 14, 10, 3, 5, 14, 12];
+
+console.log(cutTheSticks(myArray));
+console.log(cutTheSticks(myArray2));
+console.log(cutTheSticks(myArray3));
+console.log(cutTheSticks(myArray4));
