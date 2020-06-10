3891f84b680a4b347ff006774dad07439acc3c3c
Hardik Patel <hpatel@wsgc.com>
2020-06-10 15:56:38 -0700

    14,609

--- js/taum-and-bday.js
+++ js/taum-and-bday.js
@@ -4,37 +4,6 @@
  * using library like big decimal should solve this issue.
  */
 
-function taumBday1(b, w, bc, wc, z) {
-    if (bc > wc) {
-        if (bc - wc > z) {
-            bc = wc + z;
-        }
-    }
-    if (wc > bc) {
-        if (wc - bc > z) {
-            wc = bc + z;
-        }
-    }
-    return b * bc + w * wc;
-}
-
-const b = 742407782;
-const w = 90529439;
-const bc = 847666641;
-const wc = 8651519;
-const z = 821801924;
-
-console.log(taumBday1(b, w, bc, wc, z));
-
-function taumBday2(b, w, bc, wc, z) {
-
-    if (bc === wc) {
-        return b * bc + w * wc;
-    } else if (Math.abs(bc - wc) < z) {
-        return b * bc + w * wc;
-    } else if (bc + z <= wc) {
-        return (b * bc) + ((w * (bc + z)));
-    } else {
-        return (b * (wc + z)) + (w * wc);
-    }
+function taumBday(b, w, bc, wc, z) {
+    return b * Math.min(bc, wc + z) + w * Math.min(wc, bc + z);
 }
